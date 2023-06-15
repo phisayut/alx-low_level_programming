2b800ae46fd80df928b0f42cde447beecfc4aa7f
phisayut <fisayoola01@gmail.com>
2023-06-15 15:37:47 -0700

    c programing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main  -  Entry point
+ *
+ * return: Always 0(succes)
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
