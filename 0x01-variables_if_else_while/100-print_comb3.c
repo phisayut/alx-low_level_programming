b8f1549ec9551bc87cb6a45a87a3b99a120604bf
phisayut <fisayoola01@gmail.com>
2023-06-19 05:12:03 -0700

    C varaiables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 9; num1++) /*print tens place*/
+	{
+		for (num2 = 0; num2 <= 9; num2++) /*print ones place*/
+		{
+			if (!(num1 == 9 && num2 == 9)) /*skip comma at end*/
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point ,print all combinations of three different digit
+ * Result: Always 0(Sucess)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				if ((x != y) && (y != z))
+				{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
