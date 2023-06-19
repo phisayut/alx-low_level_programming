4c7d760eefd4fc51b0fc92143725a4189c817298
phisayut <fisayoola01@gmail.com>
2023-06-19 06:11:51 -0700

    c Variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,11 +17,11 @@ int main(void)
 		{
 			for (z = 0; z <= 9; z++)
 			{
-				if ((x != y) && (y != z))
-				{
 				putchar(x + '0');
 				putchar(y + '0');
 				putchar(z + '0');
+				if (x != '7' || y != '8' || y != '9')
+				{
 				putchar(',');
 				putchar(' ');
 				}
