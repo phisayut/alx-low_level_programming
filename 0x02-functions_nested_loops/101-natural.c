158d6a9bc9dc8710fa21d4579076b4aafabd7c52
phisayut <fisayoola01@gmail.com>
2023-06-21 16:01:06 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -print sum of multiples of 3 or 5 below 1024
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	int sum;
+
+	sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci nums
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count1;
+	int count2 = 50;
+	long x = 1;
+	long y = 2;
+
+	for (count1 = 1; count1 <= (count2 / 2); count1++)
+	{
+		printf("%li %li ", x, y);
+		x += y;
+		y += x;
+	}
+	if (count2 % 2 == 1)
+		printf("%li", x);
+
+	printf("\n");
+
+	return (0);
+
+}
