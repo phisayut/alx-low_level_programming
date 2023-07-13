6aeab7ad5b061e154da1c614f5dcd486d938b572
phisayut <fisayoola01@gmail.com>
2023-07-13 00:40:38 -0700

    Memory allocation

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * strtow - split strings into words
+ * @str: string value
+ * Return: NULL
+ */
+
+char **strtow(char *str)
+{
+	
