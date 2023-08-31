fc28d659fe9658c5c545e93bf325fbff866544d1
phisayur <fisayoola01@gmail.com>
2023-08-31 09:17:42 -0700

    Set value of given index to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a bit at a given index to 0.
+ * clear_bit - Sets the value of a bit at a given index to 0
  * @n: A pointer to the bit.
  * @index: The index to set the value at - indices start at 0.
  *
