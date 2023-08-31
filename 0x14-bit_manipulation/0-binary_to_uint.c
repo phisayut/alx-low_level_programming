a68f89efef9736bfdad76bf1e48f9358d77c6611
phisayur <fisayoola01@gmail.com>
2023-08-31 08:54:41 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
+ * binary_to_uint - Convert a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
  *
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
