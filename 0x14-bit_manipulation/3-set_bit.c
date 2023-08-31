1dfe6337fd0774a341acee4e6fe8b25a49361c98
phisayur <fisayoola01@gmail.com>
2023-08-31 09:15:57 -0700

    Set value of a bit to given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the value of a bit at a given index to 1.
+ * set_bit - Sets the value of a bit at a given index to 1
  * @n: number.
  * @index: The index to set the value at - indices start at 0.
  *
